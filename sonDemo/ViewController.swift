13042e7458c84695c4568b25e7a220a3eeaf0afb
zouinetami <68541868+zouinetami@users.noreply.github.com>
2020-08-24 10:36:38 +0100

    add function to test sonar cloud bugs detection

--- sonDemo/ViewController.swift
+++ sonDemo/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        testSonarCloudBugDetection()
+    }
+    
+    func testSonarCloudBugDetection()->Bool{
+        return true
     }
 
 
