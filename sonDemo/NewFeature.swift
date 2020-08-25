fa5dae57e0105b5275e6c38a25678915355e5a23
zouinetami <68541868+zouinetami@users.noreply.github.com>
2020-08-25 15:04:25 +0100

    add Newfeature File into develop branch

---
+++ sonDemo/NewFeature.swift
@@ -0,0 +1,30 @@
+//
+//  NewFeature.swift
+//  sonDemo
+//
+//  Created by stibits on 8/25/20.
+//  Copyright © 2020 stibits. All rights reserved.
+//
+
+import UIKit
+
+class NewFeature: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
