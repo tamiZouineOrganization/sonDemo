a3547473158e1a20d1c5db5d93612b1af3532f61
zouinetami <68541868+zouinetami@users.noreply.github.com>
2020-08-25 09:31:41 +0100

    same statement with condition

--- sonDemo/ViewController.swift
+++ sonDemo/ViewController.swift
@@ -13,22 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        testSonarCloudBugDetection()
+        testSonarCloudBugsDetection()
     }
-    
-    func testSonarCloudBugDetection()->Bool{
-        for index in 0...4{
+    func testSonarCloudBugsDetection() -> Bool {
+        for index in 0...4 {
             print(index)
         }
+        for identifier in 0...9 {
+            print(identifier)
+        }
+        if 1 == 1 {
+            print(1)
+        }
+        else {
+            print(1)
+        }
+        
         return true
     }
-    
       static var blueButtonText: UIColor {
-    
         return UIColor(red: 127.0/255.0, green: 181.0/255.0, blue: 1255.0/255.0, alpha: 1.0)
-        
     }
-
-
 }
-
