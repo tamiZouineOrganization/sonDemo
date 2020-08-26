13956493616b550be7fa679ac21b7be1c06e7ea9
zouinetami <68541868+zouinetami@users.noreply.github.com>
2020-08-26 15:39:27 +0100

    sds

--- sonDemo/AppDelegate.swift
+++ sonDemo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("test that this is the newbegins version")
+        print("test that this is the newbegins version  ")
         return true
     }
 
