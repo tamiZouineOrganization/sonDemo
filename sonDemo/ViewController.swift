49739cc91d2f9f704bd34471f5183384a2dc9bbe
tami Zouine <tami.zouine@gmail.com>
2020-08-27 11:04:00 +0200

    Update ViewController.swift

--- sonDemo/ViewController.swift
+++ sonDemo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         testSonarCloudBugsDetection()
+        testSonarCloudBugsDetection()
     }
     func testSonarCloudBugsDetection() -> Bool {
         for index in 0...4 {
