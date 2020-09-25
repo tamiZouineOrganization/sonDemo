4f9c6adb45f92ce5b03bfc804ba99f33539dfd31
zouinetami <68541868+zouinetami@users.noreply.github.com>
2020-09-25 18:32:33 +0100

    add bug test function

--- sonDemo/File22.swift
+++ sonDemo/File22.swift
@@ -10,3 +10,44 @@ import Foundation
 import Foundation
 import Foundation
 import Foundation
+
+
+class testCheck{
+    
+    
+    var Uppercasevalue: String? = "SonarCloud"
+    
+    func testSonarCloudBugsDetection() -> Bool {
+        for index in 0...4 {
+            print(index)
+            for identifier in 0...9 {
+                print(identifier)
+                if Uppercasevalue == "SonarCloud"{
+                    print("true")
+                }
+            }
+        }
+        
+        if 1 == 1 {
+            print(1)
+        }
+        else {
+            print(1)
+        }
+        switch Uppercasevalue {
+        case "SonarCloud":
+            print(1)
+        case "SonarQube":
+            print(2)
+        case "SonnarScanner":
+            print(3)
+        default:
+            print(0)
+        }
+        
+        return true
+    }
+}
+
+
+
